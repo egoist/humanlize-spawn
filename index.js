fb99c1576812068caa55a84378a017119ad8e995
EGOIST <0x142857@gmail.com>
2016-02-15 12:17:28 +0800

    typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var child = require('child_process')
 
 module.exports.sync = function (command) {
 	if (typeof command !== 'string') {
-		throw new TypeErroe('Expected a string')
+		throw new TypeError('Expected a string')
 	}
 	command = command.split(' ')
 	child.spawnSync(command[0], command.slice(1), {stdio: 'inherit'})
